82559114bfe5f450cc98b6faffc6a7d2d76b864b
Yates <kevinmyates@gmail.com>
2020-01-02 23:40:13 -0800

    Create Winning Grid.

--- Sudoku CS/Board.cs
+++ Sudoku CS/Board.cs
@@ -10,8 +10,12 @@ namespace Sudoku_CS
     class Board
     {
         ContentManager content;
+        Random r = new Random();
 
+        // Easy = 36+ blocks revealed. Medium = 27-36 blocks revealed. Hard = 19-26 blocks revealed.
         public int[,] blockGrid = new int[9, 9];
+
+        public int[,] winningBlockGrid = new int[9, 9];
 
 
         public Texture2D backgroundImage;
@@ -19,25 +23,87 @@ namespace Sudoku_CS
 
         SpriteFont font;
 
+        int blocksRevealed = 38;
+
         public Board(ContentManager Content)
         {
             content = Content;
 
-            //for (int i = 0; i < blockGrid.Length; i++)
-            //    for (int j = 0; j < blockGrid.Length; j++)
-            //        blockGrid[i, j] = 0;
-
             Array.Clear(blockGrid, 0, blockGrid.Length);
 
-            // tests
-            blockGrid[5, 5] = 3;
-            blockGrid[0, 7] = 7;
-            blockGrid[6, 6] = 8;
-            blockGrid[1, 2] = 4;
+            NewWinningGrid();
+
+            // 38 revealed
+            // check for collisions
+            for (int i = 0; i < blocksRevealed; i++)
+            {
+                int x = r.Next(0, 9);
+                int y = r.Next(0, 9);
+
+                blockGrid[x, y] = winningBlockGrid[x, y];
+            }
+
+
+
+
+            System.Diagnostics.Debug.WriteLine(blockGrid[5, 5]);
+
 
             backgroundPosition = new Vector2(50, 50);
 
             LoadContent();
+        }
+
+        public void NewWinningGrid()
+        {
+            HashSet<int> randomNumbersRow = new HashSet<int>();
+
+            for (int i = 1; i < 10; i++)
+                while (!randomNumbersRow.Add(r.Next(1, 10))) ;
+
+
+            // WORKING HERE
+            int count = 0;
+
+            foreach (var item in randomNumbersRow)
+            {
+                winningBlockGrid[count, 0] = item;
+                count += 1;
+            }
+
+
+
+
+
+            //foreach (var item in randomNumbers3)
+            //{
+            //    System.Diagnostics.Debug.WriteLine(item);
+            //}
+            //System.Diagnostics.Debug.WriteLine(randomNumbers3);
+        
+
+
+
+
+
+
+
+            for (int i = 1; i < 10; i++)
+            {
+
+                //randomNumbers.Add(i, r.Next(0, 1000));
+                //System.Diagnostics.Debug.WriteLine(randomNumbers);
+            }
+
+
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    for(int j = 0; j < 9; j++)
+            //    {
+            //        // delete
+            //        winningBlockGrid[i, j] = 5;
+            //    }
+            //}
         }
 
         public void LoadContent()
